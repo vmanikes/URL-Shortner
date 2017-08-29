6c51f386ca1d5bd6ed94ab0264838b379b58ef3d
vmanikes <vmanikes@hawk.iit.edu>
2017-08-29 17:37:26 -0400

    Completed except tests

--- URL_shortner.py
+++ URL_shortner.py
@@ -2,15 +2,21 @@ from flask import Flask,render_template,request
 import uuid
 import validators
 
+import redis
 
-from collections import defaultdict
 app = Flask(__name__)
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-shortened_urls = defaultdict()
+#Check database connection
+def checkConnection(r):
+    try:
+        if r.ping() == True:
+            return "200 Connection Succesful"
+    except Exception as e:
+        return e
 
 @app.route('/')
 def index():
-    print(render_template('index.html'))
     return render_template('index.html')
 
 @app.route('/shorten_url',methods=['POST'])
@@ -18,10 +24,14 @@ def shorten_url():
     url = request.form['url']
 
     if validators.url(url):
-        unique_id = str(uuid.uuid3(uuid.NAMESPACE_DNS,url))[:7]
-        shortened_urls['localhost:5000/'+unique_id] = url
 
-        return render_template('short_url.html',new_url='localhost:5000/'+unique_id,shortened_urls=shortened_urls)
+        #All the database operations from here follow atomic operations
+        pipe = r.pipeline()
+        unique_id = str(uuid.uuid3(uuid.NAMESPACE_DNS,url))[:7]
+        pipe.set('localhost:5000/'+unique_id, url)
+        result = pipe.execute()
+        print(result)
+        return render_template('short_url.html',new_url='localhost:5000/'+unique_id,result=url)
     else:
         return "Please enter a valid url"
 
